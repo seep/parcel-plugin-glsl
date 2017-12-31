01e8f58c87d64b851eddb8596503fef14432d22c
Chris Parker <seep@sent.com>
2017-12-31 01:56:02 -0500

    simplify

--- glsl-asset.js
+++ glsl-asset.js
@@ -2,13 +2,10 @@ const { Asset } = require('parcel-bundler');
 
 module.exports = class GLSLAsset extends Asset {
 
-  constructor(name, pkg, options) {
-    super(name, pkg, options);
-    this.type = 'js';
-  }
+  type = 'js';
 
   generate() {
-    return { js: `module.exports = \`${ this.contents }\`` };
+    return { js: `module.exports = \`${ this.contents }\`;` };
   }
 
 };
