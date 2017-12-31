68171b895249d289fa3bf39cff24675628599e74
Chris Parker <seep@sent.com>
2017-12-31 00:15:00 -0500

    tweak the asset definition

--- glsl-asset.js
+++ glsl-asset.js
@@ -1,11 +1,14 @@
-const Asset = require('parcel-bundler/src/Asset');
+const { Asset } = require('parcel-bundler');
 
 module.exports = class GLSLAsset extends Asset {
 
+  constructor(name, pkg, options) {
+    super(name, pkg, options);
+    this.type = 'js';
+  }
+
   generate() {
-
     return { js: `module.exports = \`${ this.contents }\`` };
-
   }
 
 };
