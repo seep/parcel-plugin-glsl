1b425507e57c7c951c0229887ffe60b1ef96be4f
Chris Parker <seep@sent.com>
2017-12-31 02:00:26 -0500

    fix syntax

--- glsl-asset.js
+++ glsl-asset.js
@@ -2,10 +2,13 @@ const { Asset } = require('parcel-bundler');
 
 module.exports = class GLSLAsset extends Asset {
 
-  type = 'js';
+  constructor(name, pkg, options) {
+    super(name, pkg, options);
+    this.type = 'js';
+  }
 
   generate() {
-    return { js: `module.exports = \`${ this.contents }\`;` };
+    return { js: `module.exports = \`${ this.contents }\`` };
   }
 
 };
